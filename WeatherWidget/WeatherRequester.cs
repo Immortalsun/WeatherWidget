bbb13e4c484f7f0b04b3e2c07a7c2d4b8f2505e5
Immortalsun <maashes.p@gmail.com>
2014-11-23 18:17:01 -0500

    Begin adding architecture for api calls

---
+++ WeatherWidget/WeatherRequester.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherWidget
+{
+    /// <summary>
+    /// Class that handles requestin the online weather API for information
+    /// </summary>
+    public class WeatherRequester
+    {
+
+        #region Fields
+        private const string ApiKey = "dcd9a09528ce089522caf1c9fbb1d";
+        private const string WeatherRequestURL = "http://api.worldweatheronline.com/free/v2/weather.ashx";
+        private const string WeatherSearchURL = "http://api.worldweatheronline.com/free/v2/search.ashx";
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructor
+
+        #endregion
+
+        #region Methods
+
+        #endregion
+
+        #region Events
+
+        #endregion
+
+    }
+}
